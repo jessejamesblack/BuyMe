f671c7d1e19e932644a47fadb46b675221b31df9
Colin Covello <37255989+colcove@users.noreply.github.com>
2018-03-21 23:19:06 -0400

    Update RegisterDao.java

--- cs336Final/WebContent/registration/WEB-INF/classes/bean/RegisterDao.java
+++ cs336Final/WebContent/registration/WEB-INF/classes/bean/RegisterDao.java
@@ -8,7 +8,7 @@ public class RegisterDao {
 		int status=0;
 		try{
 			Connection con=ConnectionProvider.getCon();
-            PreparedStatement ps=con.prepareStatement("insert into user432 values(?,?,?)");
+            PreparedStatement ps=con.prepareStatement("insert into EndUser values(?,?,?)");
 			ps.setString(1,u.getUname());
 			ps.setString(2,u.getUemail());
 			ps.setString(3,u.getUpass());
