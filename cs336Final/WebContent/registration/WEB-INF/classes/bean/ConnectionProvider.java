53c7d3615973ebd30fae1ad5e016c59e4b506949
Colin Covello <37255989+colcove@users.noreply.github.com>
2018-03-21 23:16:38 -0400

    Update ConnectionProvider.java

--- cs336Final/WebContent/registration/WEB-INF/classes/bean/ConnectionProvider.java
+++ cs336Final/WebContent/registration/WEB-INF/classes/bean/ConnectionProvider.java
@@ -6,8 +6,8 @@ public class ConnectionProvider {
 	static Connection con=null;
 	static{
 		try{
-			Class.forName(DRIVER);
-			con=DriverManager.getConnection(CONNECTION_URL,USERNAME,PASSWORD);
+			Class.forName("com.mysql.jdbc.Driver").newInstance();
+			con=DriverManager.getConnection("jdbc:mysql://cs336db.cyyfsrtrqnib.us-east-2.rds.amazonaws.com:3306/BuyMe","cmc585","cs336databse");
 			}catch(Exception e){}
 	}
 	public static Connection getCon(){
